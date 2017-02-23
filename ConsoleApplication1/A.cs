1bd7f3a4139c01b8b6516133ef4ae17bdd122cc5
markkoltai <markkoltai@gmail.com>
2017-02-23 12:31:04 +0100

    Add project files.

---
+++ ConsoleApplication1/A.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class A
+    {
+        public void S1()
+        {
+            Console.WriteLine("A::S1");
+            V1();
+                
+        }
+
+        public void S2()
+        {
+            Console.WriteLine("A:S2");
+            S1();
+        }
+
+        private void S3()
+        {
+            Console.WriteLine("A::S3");
+
+        }
+
+        protected virtual void V1()
+        {
+            Console.WriteLine("A::V1");
+            S3();
+        }
+    }
+}
---
+++ ConsoleApplication1/D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class D : B
+    {
+        public void S7()
+        {
+            Console.WriteLine("D::S7");
+            S3();
+        }
+
+        public void S8()
+        {
+            Console.WriteLine("D::S8");
+            V2();
+        }
+
+        protected void V2()
+        {
+            Console.WriteLine("D::V2");
+            S7();
+        }
+    }
+}
